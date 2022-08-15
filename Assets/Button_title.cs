5f44266c50c7947c830fcc2bb706c3024e08c4a9
Shogo-Nakasaki <74720278+Shogo-Nakasaki@users.noreply.github.com>
2022-08-15 22:16:53 +0900

    ver0.02
    タイトルシーンの仮完成

--- Assets/Button_title.cs
+++ Assets/Button_title.cs
@@ -1,14 +1,34 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Button_title : MonoBehaviour
 {
+    public GameObject credit;
     private string txt = "10_exploration";
+    
+    /// <summary>
+    /// メインタブへ進むためのボタン
+    /// </summary>
     public void ToMain()
     {
         SceneManager.LoadScene(txt);
     }
 
+    /// <summary>
+    /// クレジットタブを開けるためのボタン
+    /// </summary>
+    public void Open_Credit()
+    {
+        credit.SetActive(true);
+    }
+
+    /// <summary>
+    /// クレジットタブを閉じるためのボタン
+    /// </summary>
+    public void Close_Credit()
+    {
+        credit.SetActive(false);
+    }
 }
